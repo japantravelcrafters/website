20af7322ed71639ed4b79aebd1d559865c6b3134
japantravelcrafters <jtc_agency@yahoo.com>
2015-09-05 12:29:16 +0900

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,39 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-	
-// Create the email and send the message
-$to = 'jtc_agency@yahoo.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yahoo.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
-?>
+if(isset($_POST['emailer'])) {
+        if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) == true) {
+                $name           =   strip_tags($_POST['name']);
+                $email_address  =   $_POST['email'];
+                $phone          =   $_POST['phone'];
+                $message        =   strip_tags($_POST['message']);
+
+                // Create the email and send the message
+             // $to_alt         =   $email_address;
+                $to             =   "steve@phasehome.com";
+                $email_subject  =   "www.phasehome.com Contact Form:  $name";
+                $email_body     =   "Hello Steve. You have received a new submission from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+                $headers        =   "From: noreply@phasehome.com\n";
+                $headers        .=  "Reply-To: $email_address"; 
+
+                // Send copy to sender
+                // mail($to_alt ,$email_subject,$email_body,$headers);
+                // Send original to intended
+                if(!mail($to,$email_subject,$email_body,$headers)) { ?>
+                <div class="alert alert-danger">
+                    <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times</button>
+                    <strong>Message failed to send.</strong></div>
+                </div>
+                 <?php }
+                else { ?>
+                <div class="alert alert-success">
+                    <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times</button>
+                    <strong>Your message has been sent. </strong></div>
+                </div>
+                 <?php }
+            }
+        else { ?>
+        <div class="alert alert-danger">
+            <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times</button>
+            <strong>Email Invalid!</strong></div>
+        </div>
+         <?php }
+    } ?>
